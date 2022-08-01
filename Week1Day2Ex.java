b8c3232dd8cd7e270d1f832c72e4382b17a68146
Shahad Alaskah <56152970+ShahadAlaskah@users.noreply.github.com>
2022-08-01 17:27:36 +0300

    Add files via upload

---
+++ Week1Day2Ex.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class Week1Day2Ex {
+    public static void main(String[] args) {
+
+        //Week-01-Day-02-Ex
+        //------------------------------Ex 1
+//        String firstName="Shahad";
+//        String lastName="Alaskah";
+//        String fullName=firstName+' '+lastName;
+//        System.out.println(fullName);
+        //------------------------------Ex 2
+//        double num1=2;
+//        double num2=3;
+//        double sum =num1+num2;
+//        System.out.println("sum:"+sum);
+//        System.out.println("Total:"+(sum/2));
+       //------------------------------Ex 3
+
+//        String role="user";
+//
+//        if(role=="admin"){
+//            System.out.println("Hi admin");
+//        } else if (role=="superuser") {
+//            System.out.println("Hi superuser");
+//        } else if (role=="user") {
+//            System.out.println("Hi user");
+//        }
+        //------------------------------Ex 4
+//        for ( int i=0;i<=100;i++){
+//            System.out.println(i);
+//        }
+        //------------------------------Ex 5
+//        for ( int i=100;i>=30;i--){
+//            System.out.println(i);
+//        }
+        //------------------------------Ex 6
+//        Scanner myObj = new Scanner(System.in);
+//
+//        System.out.println("Enter Num:");
+//        int num = myObj.nextInt();
+//
+//        int result=num;
+//        for ( int i=1;i<5;i++){
+//            result=result*num;
+//        }
+//        System.out.println(result);
+        //------------------------------Ex 7
+//        for ( int i=0;i<=25;i++){
+//            if((i%2) == 0) {
+//                System.out.println(i);
+//            }
+//        }
+
+
+
+    }
+
+}
